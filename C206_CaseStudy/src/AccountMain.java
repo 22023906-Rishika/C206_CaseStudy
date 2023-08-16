b4f6f7c24556b64c3d138051c726c695174ba41a
22037707 <22037707@DESKTOP-MVJO8QL>
2023-08-16 17:20:09 +0800

    Push Account Main

---
+++ C206_CaseStudy/src/AccountMain.java
@@ -0,0 +1,14 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22037707, Aug 16, 2023 4:46:14 PM
+ */
+
+/**
+ * 
+ */
+public class AccountMain {
+
+}
