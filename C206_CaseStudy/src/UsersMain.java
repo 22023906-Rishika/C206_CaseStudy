c6295226121fc0644f73d84d8dbb6168d58d0d0a
22023906-Rishika <138425484+22023906-Rishika@users.noreply.github.com>
2023-08-17 16:47:41 +0800

    Update UsersMain.java
    usersMain refactor

--- C206_CaseStudy/src/UsersMain.java
+++ C206_CaseStudy/src/UsersMain.java
@@ -1,144 +1,128 @@
-
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 22037707, Aug 11, 2023 7:07:01 PM
- */
-
 import java.util.ArrayList;
 
 public class UsersMain {
-	public static ArrayList<Users> userList = new ArrayList<Users>();
+    private static ArrayList<User> userList = new ArrayList<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        initializeUserList();
 
-		userList.add(new Users("Alice", "Student", false, "22023303"));
-		userList.add(new Users("Bob", "Admin", true, "22013456"));
-		userList.add(new Users("Charli", "Student", false, "22347890"));
-		userList.add(new Users("Dave", "Student", false, "21034675"));
+        userMenu();
+        int subOpt = Helper.readInt("Please choose an option: ");
+        while (subOpt != 6) {
+            switch (subOpt) {
+                case 1:
+                    viewAllUsers();
+                    break;
+                case 2:
+                    addUser();
+                    break;
+                case 3:
+                    editUser();
+                    break;
+                case 4:
+                    deleteUser();
+                    break;
+                default:
+                    System.out.println("Invalid choice entered");
+                    break;
+            }
+            userMenu();
+            subOpt = Helper.readInt("Please choose an option: ");
+        }
 
-		userMenu();
-		int subOpt = Helper.readInt("Please choose an option: ");
-		while (subOpt != 6) {
-			switch (subOpt) {
-			case 1:
-				viewAllUser(userList);
-				break;
-			case 2:
-				addUser(userList, getUser(1, userList));
-				break;
-			case 3:
-				editUser(userList);
-				break;
-			case 4:
-				deleteUser(userList, getUser(2, userList));
-				break;
-			default:
-				System.out.println("Invalid choice entered");
-				break;
-			}
-			userMenu();
-			subOpt = Helper.readInt("Please choose an option: ");
-		}
+        System.out.println("Exited");
+    }
 
-		System.out.println("Exited");
-	}
+    private static void initializeUserList() {
+        userList.add(new User("Alice", "Student", false, "22023303"));
+        userList.add(new User("Bob", "Admin", true, "22013456"));
+        userList.add(new User("Charli", "Student", false, "22347890"));
+        userList.add(new User("Dave", "Student", false, "21034675"));
+    }
 
+    private static void userMenu() {
+        System.out.println("1. View all Users");
+        System.out.println("2. Add a new User");
+        System.out.println("3. Edit an existing User");
+        System.out.println("4. Delete an existing User");
+        System.out.println("6. Exit");
+        System.out.println();
+    }
 
-	public static void userMenu() {
-		System.out.println("1. View all Users");
-		System.out.println("2. Add a new User");
-		System.out.println("3. Edit an existing Usr");
-		System.out.println("4. Delete an existing User");
-		System.out.println("6. Exit");
-		System.out.println(" ");
-	}
+    private static void addUser() {
+        User user = getUserInput(1);
+        if (user != null) {
+            userList.add(user);
+            System.out.println("User successfully added");
+        } else {
+            System.out.println("User unable to add.");
+        }
+    }
 
-	public static void addUser(ArrayList<Users> userList, Users user) {
-		if (user != null) {
-			userList.add(user);
-			System.out.println("User successfully added");
-		} else {
-			System.out.println("User unable to add.");
-		}
-	}
+    private static void editUser() {
+        String userId = Helper.readString("Enter the user ID of the user you want to edit: ");
+        User user = getUserById(userId);
+        if (user != null) {
+            updateUserDetails(user);
+            System.out.println("User " + userId + " details updated.");
+        } else {
+            System.out.println("User not found.");
+        }
+    }
 
-	public static void editUser(ArrayList<Users> userList) {
-		String userId = Helper.readString("Enter the user ID of the user you want to edit: ");
-		boolean found = false;
+    private static void deleteUser() {
+        String userId = Helper.readString("Enter the user ID to delete: ");
+        User user = getUserById(userId);
+        if (user != null) {
+            userList.remove(user);
+            System.out.println(user.getName() + " successfully deleted.");
+        } else {
+            System.out.println("User not found.");
+        }
+    }
 
-		for (Users user : userList) {
-			if (user.getUserID().equalsIgnoreCase(userId)) {
-				found = true;
-				boolean admin = true;
-				String newName = Helper.readString("Enter new name: ");
-				String newRole = Helper.readString("Enter new role: ");
-				char newAdminStatus = Helper.readChar("Is the user an admin? (Y/N): ");
+    private static User getUserById(String userId) {
+        for (User user : userList) {
+            if (user.getUserID().equalsIgnoreCase(userId)) {
+                return user;
+            }
+        }
+        return null;
+    }
 
-				user.setName(newName);
-				user.setRole(newRole);
-				user.setAdmin(newAdminStatus);
+    private static void updateUserDetails(User user) {
+        String newName = Helper.readString("Enter new name: ");
+        String newRole = Helper.readString("Enter new role: ");
+        char newAdminStatus = Helper.readChar("Is the user an admin? (Y/N): ");
 
-				System.out.println("User " + userId + " details updated.");
-				break;
-			}
-		}
+        user.setName(newName);
+        user.setRole(newRole);
+        user.setAdmin(newAdminStatus);
+    }
 
-		if (!found) {
-			System.out.println("User not found.");
-		}
-	}
+    private static User getUserInput(int function) {
+        User user = null;
+        if (function == 1) {
+            String name = Helper.readString("Enter user's name: ");
+            String role = Helper.readString("Enter user's role: ");
+            boolean admin = Helper.readBoolean("Is the user an admin? (true/false): ");
+            String userId = Helper.readString("Enter user's ID: ");
+            user = new User(name, role, admin, userId);
+        }
+        return user;
+    }
 
+    private static void viewAllUsers() {
+        String format = "%-15s %-15s %-10s %-15s%n";
+        String header = String.format(format, "NAME", "ROLE", "ADMIN", "USER ID");
+        StringBuilder output = new StringBuilder(header);
 
+        for (User user : userList) {
+            String adminStatus = user.getAdmin() ? "Yes" : "No";
+            String userLine = String.format(format, user.getName(), user.getRole(), adminStatus, user.getUserID());
+            output.append(userLine);
+        }
 
-	public static void deleteUser(ArrayList<Users> userList, Users user) {
-		if (user != null) {
-			userList.remove(user);
-			System.out.println(user.getName() + " successfully deleted.");
-		} else {
-			System.out.println("User not found.");
-		}
-	}
-
-	public static Users getUser(int function, ArrayList<Users> userList) {
-		Users user = null;
-		if (function == 1) {
-			String name = Helper.readString("Enter user's name: ");
-			String role = Helper.readString("Enter user's role: ");
-			boolean admin = Helper.readBoolean("Is the user an admin? (true/false): ");
-			String userId = Helper.readString("Enter user's ID: ");
-			user = new Users(name, role, admin, userId);
-		} else if (function == 2) {
-			boolean found = false;
-			String userId = Helper.readString("Enter user ID to delete: ");
-			for (Users existingUser : userList) {
-				if (existingUser.getUserID().equalsIgnoreCase(userId)) {
-					user = existingUser;
-					found = true;
-					break;
-				}
-			}
-
-			if (!found) {
-				System.out.println("User not found.");
-			}
-		}
-
-		return user;
-	}
-
-	public static void viewAllUser(ArrayList<Users> userList) {
-		String output = String.format("%-15s %-15s %-10s %-15s\n", "NAME", "ROLE", "ADMIN", "USER ID");
-
-		for (Users user : userList) {
-			String adminStatus = user.getAdmin() ? "Yes" : "No";
-			output += String.format("%-15s %-15s %-10s %-15s\n", user.getName(), user.getRole(), adminStatus, user.getUserID());
-		}
-
-		System.out.println(output);
-	}
-
+        System.out.println(output);
+    }
 }
